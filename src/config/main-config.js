4d417cc386a9b47de58dfa0ddc68ad75db1c28c6
Layne <layneingramtaylor@gmail.com>
2019-10-15 15:24:18 -0500

    userController not defined

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -25,10 +25,10 @@ module.exports = {
     app.use(flash());
     app.use(logger("dev"));
     // passportConfig.init(app);
-    app.use((req, res, next) => {
-      res.locals.currentUser = req.user;
-      next();
-    });
+    // app.use((req, res, next) => {
+    //   res.locals.currentUser = req.user;
+    //   next();
+    // });
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,5 @@
 module.exports = {
   index(req, res, next) {
-    res.render("static/index", { title: "!!!!!!!" });
+    res.render("static/index", { title: "Welcome to Blocipedia" });
   }
 };
