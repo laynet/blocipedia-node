575041e7d7cbabe99823192a273be9dcc5185448
Layne <layneingramtaylor@gmail.com>
2019-10-20 17:19:39 -0500

    trouble with sendgrid

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,6 +1,8 @@
 // #1
 const User = require("./models").User;
 const bcrypt = require("bcryptjs");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   // #2
@@ -16,6 +18,14 @@ module.exports = {
       password: hashedPassword
     })
       .then(user => {
+        const msg = {
+          to: "test@example.com",
+          from: "test@example.com",
+          subject: "Sending with Twilio SendGrid is Fun",
+          text: "and easy to do anywhere, even with Node.js",
+          html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+        };
+        sgMail.send(msg);
         callback(null, user);
       })
       .catch(err => {
