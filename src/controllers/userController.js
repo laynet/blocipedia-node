2a45895e1daabad344fe0885abc72cdcb4233eab
Layne <layneingramtaylor@gmail.com>
2019-10-20 14:45:45 -0500

    cannot POST users/sign_up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,23 @@ module.exports = {
     res.render("users/signup");
   },
   create(req, res, next) {
-    //new user here
+    let newUser = {
+      username: req.body.username,
+      emaile: req.body.email,
+      password: req.body.password,
+      passwordConfirmation: req.body.passwordConfirmation
+    };
+    userQueries.createUser(newUser, (err, user) => {
+      if (err) {
+        req.flash("error", err);
+        res.redirect("/users/sign_up");
+      } else {
+        // #3
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've successfully signed in!");
+          res.redirect("/");
+        });
+      }
+    });
   }
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,6 @@ const validation = require("./validation");
 const userController = require("../controllers/userController");
 
 router.get("/users/sign_up", userController.signUp);
-router.post("/users", validation.validateUsers, userController.create);
+router.post("/users/sign_up", validation.validateUsers, userController.create);
 
 module.exports = router;
