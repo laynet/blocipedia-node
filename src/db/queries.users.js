90760148db023fa5f58989ef3df25dd5f9f065c6
Layne <layneingramtaylor@gmail.com>
2019-10-20 19:15:13 -0500

    WIP commit

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -18,13 +18,16 @@ module.exports = {
       password: hashedPassword
     })
       .then(user => {
+        console.log("USER EMAIL : ", newUser.email);
         const msg = {
-          to: "test@example.com",
+          to: newUser.email,
+
           from: "test@example.com",
           subject: "Sending with Twilio SendGrid is Fun",
           text: "and easy to do anywhere, even with Node.js",
           html: "<strong>and easy to do anywhere, even with Node.js</strong>"
         };
+
         sgMail.send(msg);
         callback(null, user);
       })
