cced13629e997cc7c3f6c418060f2a731a8b20d2
Layne <layneingramtaylor@gmail.com>
2019-10-22 16:36:19 -0500

    checkpoint user sign-up complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   signUp(req, res, next) {
@@ -14,15 +16,21 @@ module.exports = {
     };
     userQueries.createUser(newUser, (err, user) => {
       if (err) {
-        console.log("ERROR: ", err);
         req.flash("error", err);
         res.redirect("/users/sign_up");
       } else {
-        // #3
         passport.authenticate("local")(req, res, () => {
           req.flash("notice", "You've successfully signed in!");
           res.redirect("/");
         });
+        const msg = {
+          to: newUser.email,
+          from: "test@example.com",
+          subject: "Sending with Twilio SendGrid is Fun",
+          text: "and easy to do anywhere, even with Node.js",
+          html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+        };
+        sgMail.send(msg);
       }
     });
   }
