34fd227fdd5422973c1ab371b638db9b59d83186
Layne <layneingramtaylor@gmail.com>
2019-10-22 14:40:54 -0500

    going back to a different commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,6 @@
 const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   signUp(req, res, next) {
@@ -17,21 +16,10 @@ module.exports = {
         req.flash("error", err);
         res.redirect("/index");
       } else {
-        const msg = {
-          to: newUser.email,
-
-          from: "layneingramtaylor@gmail.com",
-          subject: "Sending with Twilio SendGrid is Fun",
-          text: "and easy to do anywhere, even with Node.js",
-          html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-        };
-
-        sgMail.send(msg);
-
-        // passport.authenticate("local")(req, res, () => {
-        req.flash("notice", "You've successfully signed in!");
-        res.redirect("/");
-        // });
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've successfully signed in!");
+          res.redirect("/");
+        });
       }
     });
   }
