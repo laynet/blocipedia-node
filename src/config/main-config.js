b3f0584cd31fd727729615d98083c65d68bd28de
Layne <layneingramtaylor@gmail.com>
2019-10-22 15:04:35 -0500

    app is working before integrating sendgrid

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const session = require("express-session");
 const flash = require("express-flash");
-// const passportConfig = require("./passport-config");
+const passportConfig = require("./passport-config");
 const logger = require("morgan");
 
 module.exports = {
@@ -24,7 +24,7 @@ module.exports = {
     );
     app.use(flash());
     app.use(logger("dev"));
-    // passportConfig.init(app);
+    passportConfig.init(app);
     app.use((req, res, next) => {
       res.locals.currentUser = req.user;
       next();
