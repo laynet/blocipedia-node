f2376457ee1335ca3b4e2117ef0a20429364545d
Layne <layneingramtaylor@gmail.com>
2019-10-24 11:10:04 -0500

    got it back to just redirecting to unauthorized when using wrong sign in info

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,7 +42,7 @@ module.exports = {
     passport.authenticate("local")(req, res, () => {
       if (!req.user) {
         req.flash("notice", "Sign in failed. Please try again.");
-        res.redirect("/users/sign_in");
+        res.redirect("/");
       } else {
         req.flash("notice", "You've successfully signed in!");
         res.redirect("/");
