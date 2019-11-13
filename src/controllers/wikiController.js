1a6fe1f7095e842abc58158501702654d0b37e1d
Layne <layneingramtaylor@gmail.com>
2019-11-13 13:59:55 -0600

    added wiki policy back

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,5 +1,5 @@
 const wikiQueries = require("../db/queries.wikis.js");
-const Authorizer = require("../policies/application");
+const Authorizer = require("../policies/wiki");
 
 module.exports = {
   publicIndex(req, res, next) {
---
+++ src/policies/wiki.js
@@ -0,0 +1,19 @@
+const ApplicationPolicy = require("./application");
+
+module.exports = class WikiPolicy extends ApplicationPolicy {
+  new() {
+    return this._isStandard() || this._isPremium();
+  }
+  create() {
+    return this.new();
+  }
+  edit() {
+    return this._isStandard() || this._isPremium();
+  }
+  update() {
+    return this.edit();
+  }
+  destroy() {
+    return this.update();
+  }
+};
