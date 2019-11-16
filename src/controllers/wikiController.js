b92b48257519a19bf3511e6ba2f5d6e5f12b7b69
Layne <layneingramtaylor@gmail.com>
2019-11-16 13:59:13 -0600

    markdown checkpoint complete

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,5 +1,6 @@
 const wikiQueries = require("../db/queries.wikis.js");
 const Authorizer = require("../policies/wiki");
+const markdown = require("markdown").markdown;
 
 module.exports = {
   index(req, res, next) {
@@ -56,6 +57,7 @@ module.exports = {
       if (err || wiki == null) {
         res.redirect(404, "/");
       } else {
+        wiki.body = markdown.toHTML(wiki.body);
         res.render("wikis/show", { wiki });
       }
     });
