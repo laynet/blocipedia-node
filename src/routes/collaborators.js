95ff9e40974a6b2a21644cbb94f06aede226da45
Layne <layneingramtaylor@gmail.com>
2019-11-18 17:00:02 -0600

    edit page rendering - logic wasn't working(wiki was not being defined)

--- src/routes/collaborators.js
+++ src/routes/collaborators.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const collaboratorController = require("../controllers/collaboratorController");
 
-// router.post("/wikis/:wikiId/collaborators/add", collaboratorController.add);
+router.post("/wikis/:wikiId/collaborators/add", collaboratorController.add);
 router.get("/wikis/:wikiId/collaborators", collaboratorController.add);
+// router.post(
+//   "/wikis/:wikiId/collaborators/remove",
+//   collaboratorController.remove
+// );
+
 module.exports = router;
