753aebbafe2239ff3be1693a28022f886de133db
Layne <layneingramtaylor@gmail.com>
2019-11-20 15:07:13 -0600

    kill me

--- src/controllers/collaboratorController.js
+++ src/controllers/collaboratorController.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const wikiQueries = require("../db/queries.wikis.js");
 const collaboratorQueries = require("../db/queries.collaborators.js");
 const Authorizer = require("../policies/application");
-const wikis = wikiQueries.getWiki();
 
 module.exports = {
   //   add(req, res, next) {
   //     res.render("collaborators/edit");
   //   },
   edit(req, res, next) {
+    const wikis = wikiQueries.getWiki(req.params.id);
+    console.log("%%%%%%%WIKIS", wikis);
     res.render("collaborators/edit", { wikis });
   }
 };
