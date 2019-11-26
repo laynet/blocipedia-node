3303f378b899be78f411463d45498ee47f165ef6
Layne <layneingramtaylor@gmail.com>
2019-11-26 16:47:50 -0600

    update queries

--- src/db/queries.collaborators.js
+++ src/db/queries.collaborators.js
@@ -6,23 +6,48 @@ const Authorizer = require("../policies/application");
 module.exports = {
   add(req, callback) {
     console.log("COLLAB QUERIES REQ USER ", req);
-    if (req.user.username === req.body.collaborator) {
-      return callback("You cannot add yourself to collaborators!");
+    if (req.user.username == req.body.collaborator) {
+      return callback("Cannot add yourself to collaborators!");
     }
     User.findAll({
       where: {
         username: req.body.collaborator
       }
-    }).then(users => {
-      if (!users[0]) {
-        return callback("User not found.");
-      }
-      Collaborator.findAll({
-        where: {
-          userId: users[0].id,
-          wikiId: req.params.wikiId
+    })
+      .then(users => {
+        if (!users[0]) {
+          return callback("User not found.");
         }
+        Collaborator.findAll({
+          where: {
+            userId: users[0].id,
+            wikiId: req.params.wikiId
+          }
+        })
+          .then(collaborators => {
+            if (collaborators.length != 0) {
+              return callback(
+                `${req.body.collaborator} is already a collaborator on this wiki.`
+              );
+            }
+            let newCollaborator = {
+              userId: users[0].id,
+              wikiId: req.params.wikiId
+            };
+            return Collaborator.create(newCollaborator)
+              .then(collaborator => {
+                callback(null, collaborator);
+              })
+              .catch(err => {
+                callback(err, null);
+              });
+          })
+          .catch(err => {
+            callback(err, null);
+          });
+      })
+      .catch(err => {
+        callback(err, null);
       });
-    });
   }
 };
